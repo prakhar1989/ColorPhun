3167f84074b2ce3dd2b344df0ecd7d2caebb96bf
Prakhar Srivastav <prakhar1989@gmail.com>
2014-08-17 23:17:07 +0300

    Added points logic

--- app/src/main/java/com/example/prakharsriv/colorphun/MainActivity.java
+++ app/src/main/java/com/example/prakharsriv/colorphun/MainActivity.java
@@ -2,15 +2,21 @@ package com.example.prakharsriv.colorphun;
 
 import android.app.Activity;
 import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Pair;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity implements View.OnClickListener {
 
     private Button topBtn, bottomBtn;
+    private TextView pointsTextView, levelTextView;
     private int level;
+    private int points;
+    private final int POINT_INCREMENT = 20;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,11 +26,17 @@ public class MainActivity extends Activity implements View.OnClickListener {
         topBtn = (Button) findViewById(R.id.top_button);
         bottomBtn = (Button) findViewById(R.id.bottom_button);
 
+        pointsTextView = (TextView) findViewById(R.id.points_value);
+        levelTextView = (TextView) findViewById(R.id.level_value);
+
+        // initialize the crew
+        resetGame();
+
+        // set the stage
         topBtn.setOnClickListener(this);
         bottomBtn.setOnClickListener(this);
 
-        // initialize level
-        level = 1;
+        // begin the show
         setColorsOnButtons();
     }
 
@@ -34,8 +46,35 @@ public class MainActivity extends Activity implements View.OnClickListener {
         bottomBtn.setBackgroundColor(colorPair.second);
     }
 
+    private void resetGame() {
+        level = 1;
+        points = 0;
+        pointsTextView.setText(Integer.toString(points));
+        levelTextView.setText(Integer.toString(level));
+    }
+
     @Override
     public void onClick(View view) {
+
+        Drawable clickedBtnBg = view.getBackground();
+        Drawable unclickedBtnBg;
+        if (view == topBtn) {
+            unclickedBtnBg = bottomBtn.getBackground();
+        } else {
+            unclickedBtnBg = topBtn.getBackground();
+        }
+
+        int alpha1 = clickedBtnBg.getAlpha();
+        int alpha2 = unclickedBtnBg.getAlpha();
+
+        if (alpha1 > alpha2) {
+            points = points + POINT_INCREMENT;
+            pointsTextView.setText(Integer.toString(points));
+        } else {
+            Toast.makeText(this, "Game over!", Toast.LENGTH_SHORT).show();
+            resetGame();
+        }
+
         setColorsOnButtons();
     }
 
@@ -48,7 +87,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
         // TODO: Improve the formula for alphas
         int alpha1 = 200 + (int)(Math.random() * 55);
         int delta = (10 - level) * 2;
-        int alpha2 = alpha1 > 227 ? alpha1 - delta : alpha1 + delta;
+        int alpha2 = alpha1 + delta * (alpha1 > 227 ? -1 : 1);
 
         int color1 = Color.argb(alpha1, red, green, blue);
         int color2 = Color.argb(alpha2, red, green, blue);
