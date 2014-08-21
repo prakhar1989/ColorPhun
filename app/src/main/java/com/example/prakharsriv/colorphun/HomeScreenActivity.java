e66a5e5894cce687e0f7698cdb6cf208941850ea
Prakhar Srivastav <prakhar1989@gmail.com>
2014-08-21 08:28:11 +0300

    New activity for Home Screen

---
+++ app/src/main/java/com/example/prakharsriv/colorphun/HomeScreenActivity.java
@@ -0,0 +1,22 @@
+package com.example.prakharsriv.colorphun;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+public class HomeScreenActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_screen);
+    }
+
+    public void playGame(View view) {
+
+    }
+
+    public void viewScoreBoard(View view) {
+
+    }
+}
