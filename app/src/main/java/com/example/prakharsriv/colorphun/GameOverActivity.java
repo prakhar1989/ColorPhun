aa7c5664bf27ecafc272134bfe8071ebb2f35b37
Prakhar Srivastav <prakhar1989@gmail.com>
2014-08-27 21:08:44 +0300

    Added sexy counter animations to Points :metal:

--- app/src/main/java/com/example/prakharsriv/colorphun/GameOverActivity.java
+++ app/src/main/java/com/example/prakharsriv/colorphun/GameOverActivity.java
@@ -1,29 +1,34 @@
 package com.example.prakharsriv.colorphun;
 
+import android.animation.ValueAnimator;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.BounceInterpolator;
+import android.view.animation.DecelerateInterpolator;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class GameOverActivity extends Activity {
 
-    private int points;
+    private int points, best;
+    private TextView gameOverText, pointsBox, bestBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_over);
 
-        TextView gameOverText = (TextView) findViewById(R.id.game_over);
+        gameOverText = (TextView) findViewById(R.id.game_over);
         TextView levelIndicator = (TextView) findViewById(R.id.level_indicator);
-        TextView pointsBox = (TextView) findViewById(R.id.points_box);
+        pointsBox = (TextView) findViewById(R.id.points_box);
         TextView bestLabel = (TextView) findViewById(R.id.best_label);
-        TextView bestBox = (TextView) findViewById(R.id.best_box);
+        bestBox = (TextView) findViewById(R.id.best_box);
         Button replayBtn = (Button) findViewById(R.id.replay_btn);
 
+        // setting up typeface
         Typeface avenir_black = Typeface.createFromAsset(getAssets(), "fonts/avenir_black.ttf");
         Typeface avenir_book = Typeface.createFromAsset(getAssets(), "fonts/avenir_book.ttf");
 
@@ -38,11 +43,51 @@ public class GameOverActivity extends Activity {
         Bundle bundle = getIntent().getExtras();
         points = bundle.getInt("points");
         int level = bundle.getInt("level");
-        int best = bundle.getInt("best");
+        best = bundle.getInt("best");
         pointsBox.setText(String.format("%03d", points));
         bestBox.setText(String.format("%03d", best));
         levelIndicator.setText("Level " + Integer.toString(level));
 
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        if (hasFocus) {
+            ValueAnimator pointsAnim = getCounterAnimator(pointsBox, points);
+            pointsAnim.setDuration(1200);
+
+            ValueAnimator gameOverAnim = getSlidingAnimator();
+            gameOverAnim.setDuration(500);
+
+            // start animations
+            gameOverAnim.start();
+            pointsAnim.start();
+        }
+    }
+
+    ValueAnimator getCounterAnimator(final TextView view, final int maxValue) {
+        ValueAnimator anim = ValueAnimator.ofInt(0, 1);
+        anim.setInterpolator(new DecelerateInterpolator());
+        anim.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                int val = (int) (maxValue * valueAnimator.getAnimatedFraction());
+                view.setText(String.format("%03d", val));
+            }
+        });
+        return anim;
+    }
+
+    ValueAnimator getSlidingAnimator() {
+        ValueAnimator anim = ValueAnimator.ofFloat(0, 1);
+        anim.setInterpolator(new BounceInterpolator());
+        anim.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                gameOverText.setY(130 * valueAnimator.getAnimatedFraction());
+            }
+        });
+        return anim;
     }
 
     public void playGame(View view) {
